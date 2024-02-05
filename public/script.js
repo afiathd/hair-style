840c32b06cf0ef60a0ad2e9c1a3d88b439780ec0
afiathd <afiath.d@gmail.com>
2024-02-05 19:16:05 +0100

    some repairs 5  commit

--- public/script.js
+++ public/script.js
@@ -1,5 +1,50 @@
 
-//document.querySelector('#link5').click();
+const images = document.querySelectorAll('.sContent');
+
+const active = document.querySelector('.active');
+
+
+let counter = 0;
+
+checkImage(images, active, counter);
+
+function checkImage(images, active, counter) {
+
+    
+    images.forEach(image => {
+
+        let styles = window.getComputedStyle(image);
+    
+        if(styles.display == 'block'){
+
+            counter++;
+
+            if(counter == 2){ 
+                   
+                active.classList.remove('active');
+            }
+    
+            return counter
+        }
+    
+    }) 
+
+
+}
+
+
+const links = document.querySelectorAll('.link');
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        counter++;
+        checkImage(images, active, counter)})
+});
+
+
+
+
+
 
 const booking = document.querySelector('#booking');
 
